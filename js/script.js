a3a5558f2b2070bebb9682ce727199ce57ce9ed1
Thiago <thiagotesche@gmail.com>
2021-10-08 11:05:41 -0300

    customizando a pagina e colocando um menu ancora

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
+
+
 $('#btn-linkedin').click(function(){
     window.open("https://www.linkedin.com/in/thiago-tesche-996b52213/");
+});
+
+$('#open-linkedin-icon').click(function(){
+    window.open("https://www.linkedin.com/in/thiago-tesche-996b52213/");
+});
+
+$('#open-github-icon').click(function(){
+    window.open("https://github.com/ThiagoTesche/");
+});
+
+$("#open-instagram-icon").click(function(){
+    window.open("instagram.com/thiago_tesche/");
+});
+
+$("#btn-whatsapp-open").click(function(){
+    window.open("https://api.whatsapp.com/send?phone=5551989518849");
+});
+
+$('#sobre-mim-menu').click(function(){
+    var about = document.getElementById("about");
+    about.scrollIntoView();
+
+});
+
+$("#habilidades-menu").click(function(){
+    var habilidade = document.getElementById("habilidades-view");
+    habilidade.scrollIntoView();
+});
+
+$("#projetos-menu").click(function(){
+    var projetos = document.getElementById("projetos-view");
+    projetos.scrollIntoView();
 });
 
